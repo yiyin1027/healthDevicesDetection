d41e3f6ccbd5678b8221a8afcd103a3d86daf740
--replace-all <yzhan647@uottawa.ca>
2020-03-29 20:21:18 -0400

    Updates

---
+++ precache-manifest.8460dc139168128099b954f31f641da8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13aa9d7c2048f465edc4aa2058f7631",
+    "url": "/healthDevicesDetection/index.html"
+  },
+  {
+    "revision": "470ef6bfb674b8c55cd6",
+    "url": "/healthDevicesDetection/static/css/3.f7d54674.chunk.css"
+  },
+  {
+    "revision": "fab103e8a8db09c23cec",
+    "url": "/healthDevicesDetection/static/css/main.ef39c764.chunk.css"
+  },
+  {
+    "revision": "470ef6bfb674b8c55cd6",
+    "url": "/healthDevicesDetection/static/js/3.5ba43843.chunk.js"
+  },
+  {
+    "revision": "f65549ba603ff4654e7ff71af428cd3f",
+    "url": "/healthDevicesDetection/static/js/3.5ba43843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d1839958d42a401fc9",
+    "url": "/healthDevicesDetection/static/js/4.3b508f4a.chunk.js"
+  },
+  {
+    "revision": "a42ca52405e530f7a974",
+    "url": "/healthDevicesDetection/static/js/5.1f002238.chunk.js"
+  },
+  {
+    "revision": "04372ffddb627afc026b",
+    "url": "/healthDevicesDetection/static/js/6.68eaf271.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/healthDevicesDetection/static/js/6.68eaf271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab103e8a8db09c23cec",
+    "url": "/healthDevicesDetection/static/js/main.f5448350.chunk.js"
+  },
+  {
+    "revision": "f534d9055c8c364aba20",
+    "url": "/healthDevicesDetection/static/js/runtime-main.743c9c30.js"
+  },
+  {
+    "revision": "4b759b499a028da9d533",
+    "url": "/healthDevicesDetection/static/js/xlsx.6c8c99ca.chunk.js"
+  },
+  {
+    "revision": "310cd936f4317fbd0a7fbfc45d67d6fe",
+    "url": "/healthDevicesDetection/static/media/Fallpicture.310cd936.png"
+  },
+  {
+    "revision": "37e094e8a67244f30cb8ebe2427c752b",
+    "url": "/healthDevicesDetection/static/media/home1.37e094e8.png"
+  },
+  {
+    "revision": "b976e016e57e78d206d6e52d98eaa521",
+    "url": "/healthDevicesDetection/static/media/home12.b976e016.jpg"
+  },
+  {
+    "revision": "0899ac6e46f7734bd1ed99a3656357bb",
+    "url": "/healthDevicesDetection/static/media/image1.0899ac6e.jpeg"
+  }
+]);
---
+++ static/js/xlsx.6c8c99ca.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpproject-web"]=this["webpackJsonpproject-web"]||[]).push([[2],{1921:function(n,c){},1993:function(n,c){},1994:function(n,c){}}]);
+//# sourceMappingURL=xlsx.6c8c99ca.chunk.js.map
